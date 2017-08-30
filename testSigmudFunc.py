916bd38fa63ac1ba776e9d622e42c479375ebec2
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-08-30 13:32:44 -0500

    configure value function from web form

--- testSigmudFunc.py
+++ testSigmudFunc.py
@@ -1,20 +1,23 @@
 import math
-
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import StringIO
-
 import numpy
+
+from jinja2 import Environment, FileSystemLoader
+
 from flask import Flask, make_response, request
 app = Flask(__name__)
+
+env = Environment(loader=FileSystemLoader('templates'))
 
 
 def logistica(t, L, k, center):
     return L / (1.0 + math.exp(-k * (t - center)))
 
 
-@app.route("/logistica/")
-def l():
+@app.route("/plot_logistica/")
+def plot_logistica():
 
     x = numpy.linspace(2200, 3000, 100)  # 100 linearly spaced numbers
     L = float(request.args.get('L', 1))
@@ -33,6 +36,12 @@ def l():
     response = make_response(png_output.getvalue())
     response.headers['Content-Type'] = 'image/png'
     return response
+
+
+@app.route("/")
+def form():
+    template = env.get_template('base.html')
+    return template.render()
 
 
 if __name__ == "__main__":
