364baaed2690be951192b9f9acfe6f02662d84b6
Fidel Serrano <serranoycandela@gmail.com>
2017-08-31 12:13:47 -0500

    modification of value function modifies the map

--- static/styles.js
+++ static/styles.js
@@ -1,12 +1,12 @@
-var webber_cuts = [0,33,66,98,131,164];
+var webber_cuts = [0.0,0.2,0.4,0.6,0.8,1.0];
 var paleta = ['rgba(74,190,181,0.8)', 'rgba(24,138,156,0.8)', 'rgba(0,69,132,0.8)', 'rgba(0,30,123,0.8)', 'rgba(16,0,90,0.8)'];
 var borde = 'rgba(255,255,255,0)';
-var styleCache_encharcamientos_ageb0={};
+var styleCache_ageb0={};
 
-var style_encharcamientos_ageb0 = function(feature, resolution) {
-    var ranges_encharcamientos_ageb0 = [[webber_cuts[0], webber_cuts[1], [ new ol.style.Style({
-	stroke: new ol.style.Stroke({color: borde, lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),
-	fill: new ol.style.Fill({color: paleta[0]})})]],
+var style_ageb0 = function(feature, resolution) {
+    var ranges_ageb0 = [[webber_cuts[0], webber_cuts[1], [ new ol.style.Style({
+	                                    stroke: new ol.style.Stroke({color: borde, lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),
+	                                    fill: new ol.style.Fill({color: paleta[0]})})]],
 					[webber_cuts[1], webber_cuts[2], [ new ol.style.Style({
 					    stroke: new ol.style.Stroke({color: borde, lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),
 					    fill: new ol.style.Fill({color: paleta[1]})})]],
@@ -20,13 +20,37 @@ var style_encharcamientos_ageb0 = function(feature, resolution) {
 					    stroke: new ol.style.Stroke({color: borde, lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),
 					    fill: new ol.style.Fill({color: paleta[4]})
 					})]]];
-    var key = "Total_ench";                                                                   
-    var style = ranges_encharcamientos_ageb0[0][2];
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("fv");
+     var style = ranges_ageb0[0][2];
+            for (i = 0; i < ranges_ageb0.length; i++){
+                var range = ranges_ageb0[i];
+                if (value > range[0] && value<=range[1]){
+		    
+                    style =  range[2];
+		    
+                }
+            };
     
-    if (!styleCache_encharcamientos_ageb0[key]){
+    var labelText = "";
+    var key = "";
+    size = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    if ("" !== null) {
+        labelText = String("");
+    } else {
+        labelText = ""
+    }                                                            
+    
+    if (!styleCache_ageb0[key]){
 	var text = new ol.style.Text({
 	    font: '14.3px \'Ubuntu\', sans-serif',
-	    
+	    text: labelText,
 	    textBaseline: "center",
 	    textAlign: "left",
 	    offsetX: 5,
@@ -35,36 +59,52 @@ var style_encharcamientos_ageb0 = function(feature, resolution) {
 		color: 'rgba(0, 0, 0, 255)'
 	    }),
 	});
-	styleCache_encharcamientos_ageb0[key] = new ol.style.Style({"text": text})
+	styleCache_ageb0[key] = new ol.style.Style({"text": text})
     }
-    var allStyles = [styleCache_encharcamientos_ageb0[key]];
+    var allStyles = [styleCache_ageb0[key]];
     allStyles.push.apply(allStyles, style);
     return allStyles;
 };
 
-var format_encharcamientos_ageb0 = new ol.format.GeoJSON();
-var features_encharcamientos_ageb0 = format_encharcamientos_ageb0.readFeatures(geojson_encharcamientos_ageb0,
-									       {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_encharcamientos_ageb0 = new ol.source.Vector({
+var format_ageb0 = new ol.format.GeoJSON();
+var features_ageb0 = format_ageb0.readFeatures(json_elev,{dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_ageb0 = new ol.source.Vector({
     attributions: [new ol.Attribution({html: '<a href=""></a>'})],
 });
 
-jsonSource_encharcamientos_ageb0.addFeatures(features_encharcamientos_ageb0);
+jsonSource_ageb0.addFeatures(features_ageb0);
 
-var lyr_encharcamientos_ageb0 = new ol.layer.Vector({
-    source:jsonSource_encharcamientos_ageb0,
-    style: style_encharcamientos_ageb0,
-    title: "enfermedadgastro",
+var lyr_elev = new ol.layer.Vector({
+    source:jsonSource_ageb0,
+    style: style_ageb0,
+    title: "altura",
     opacity: 0.85
 });
 
-console.log('huevos');
-console.log(lyr_encharcamientos_ageb0);
+function logistica(x){
+    var L = $('#L').val(),
+    k = $('#k').val(),
+    center = $('#center').val();
+    return L / (1.0 + Math.exp(-k * (x - center)))
+    
+}
+
+function recalculate(){
+    jsonSource_ageb0.getFeatures().forEach(function(feature){
+       feature.setProperties({
+           fv: logistica(feature.get("ELEV"))
+       });
+    });
+    lyr_elev.setStyle(style_ageb0);
+    //lyr_elev.redraw();
+    
+}
 
 function setIntervals(cuts){
     webber_cuts = cuts;
-    lyr_encharcamientos_ageb0.setStyle(style_encharcamientos_ageb0);
-    // lyr_encharcamientos_ageb0.redraw();
+    lyr_elev.setStyle(style_ageb0);
+    lyr_elev.redraw();
+   
 }
 
 var stamenLayer = new ol.layer.Tile({
@@ -73,7 +113,7 @@ var stamenLayer = new ol.layer.Tile({
     })
 });
 
-map = new ol.Map({
+var map = new ol.Map({
     projection:"EPSG:4326",
     layers: [stamenLayer],
     target: 'map',
@@ -85,4 +125,4 @@ map = new ol.Map({
 
 
 
-map.addLayer(lyr_encharcamientos_ageb0);
+map.addLayer(lyr_elev);
