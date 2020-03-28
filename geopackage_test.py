d642bbc4b9a195dfc77d6dd574682638c34f6215
Fidel Serrano <serranoycandela@gmail.com>
2020-03-28 14:24:09 -0600

    field in header and geopackage_test

---
+++ geopackage_test.py
@@ -0,0 +1,11 @@
+import gdaltools
+gdaltools.Wrapper.BASEPATH = "/Users/fidel/anaconda2/envs/geo-value-function/bin/"
+in_gpkg = r"/Users/fidel/Documents/GitHub/megadapt/src/r/megadaptr/inst/rawdata/censusblocks/megadapt_wgs84_v16.gpkg"
+in_shp = "/Users/fidel/geo-value-function/uploads/critical_zones.shp"
+out_geojson = "/Users/fidel/geo-value-function/m_16.geojson"
+
+ogr = gdaltools.ogr2ogr()
+ogr.set_encoding("UTF-8")
+ogr.set_input(in_gpkg, srs="EPSG:4326")
+ogr.set_output(out_geojson)
+ogr.execute()
