bcd4ab6b3bec0351c2b018ca4aa8b065f5db2464
Fidel Serrano <serranoycandela@gmail.com>
2020-03-28 14:45:46 -0600

    Update geopackage_test.py

--- geopackage_test.py
+++ geopackage_test.py
@@ -2,7 +2,7 @@ import gdaltools
 gdaltools.Wrapper.BASEPATH = "/Users/fidel/anaconda2/envs/geo-value-function/bin/"
 in_gpkg = r"/Users/fidel/Documents/GitHub/megadapt/src/r/megadaptr/inst/rawdata/censusblocks/megadapt_wgs84_v16.gpkg"
 in_shp = "/Users/fidel/geo-value-function/uploads/critical_zones.shp"
-out_geojson = "/Users/fidel/geo-value-function/m_16.geojson"
+out_geojson = "/Users/fidel/geo-value-function/m_17.json"
 
 ogr = gdaltools.ogr2ogr()
 ogr.set_encoding("UTF-8")
